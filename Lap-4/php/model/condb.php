251d77d75ab9134c79bfa663fc359b36799e9e16
SuPapy <640112418012@bru.ac.th>
2022-12-27 16:42:34 +0700

    Lap-4

--- Lap-4/php/model/condb.php
+++ Lap-4/php/model/condb.php
@@ -1,15 +1,12 @@
 <?php
-
 $host = "db";
 $user = "root";
 $pass = "1234";
 $db = "db_test";
 
-try{
-    $link = mysqli_connect($host,$user,$pass,$db);
-    mysqli_query($link, "SET NAMES utf8");
+try {
+    mysqli_connect($host, $user, $pass, $db);
+    mysqli_query($link, "SET NAME utf8")
 } catch (Exception $e) {
-    echo $e . ":Error no:" . mysqli_errno($link);
+    echo $e . "Error no:" . mysqli_errno($link);
 }
-
-?>
